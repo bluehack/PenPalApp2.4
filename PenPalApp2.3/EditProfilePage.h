cefa3c6c7e1710136533bbb5045a1b1041bb18e4
Sebastian Bruce <MacBook@Computers-MacBook-Air.local>
2015-06-04 10:55:58 -0500

    first commit

---
+++ PenPalApp2.3/EditProfilePage.h
@@ -0,0 +1,13 @@
+//
+//  EditProfilePage.h
+//  PenPalApp2.3
+//
+//  Created by Computer on 6/4/15.
+//  Copyright (c) 2015 Computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfilePage : UIViewController
+
+@end
---
+++ PenPalApp2.3/EditProfilePage.m
@@ -0,0 +1,37 @@
+//
+//  EditProfilePage.m
+//  PenPalApp2.3
+//
+//  Created by Computer on 6/4/15.
+//  Copyright (c) 2015 Computer. All rights reserved.
+//
+
+#import "EditProfilePage.h"
+
+@interface EditProfilePage ()
+
+@end
+
+@implementation EditProfilePage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
